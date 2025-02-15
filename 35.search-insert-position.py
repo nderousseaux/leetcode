9aac670906c93182b941c9b952d4595bdbccb9d5
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-02-15 15:34:45 +0100

    ✨ feat: problem n°35

---
+++ 35.search-insert-position.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=35 lang=python3
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if target <= nums[i]:
+                return i
+        return len(nums)
+        
+# @lc code=end
+
