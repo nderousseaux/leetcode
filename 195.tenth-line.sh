11d7b931811c57cfc3ba380f7a491ea7b4734d62
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-03-03 15:10:17 +0100

    ✨ feat: problem No.195

---
+++ 195.tenth-line.sh
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=195 lang=bash
+#
+# [195] Tenth Line
+#
+
+# @lc code=start
+# Read from the file file.txt and output the tenth line to stdout.
+awk 'NR == 10' file.txt
+
+# @lc code=end
+
