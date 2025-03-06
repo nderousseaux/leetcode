ca82ec826e7c15c118b5ddbf5fbca3a11ddd46b6
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-03-06 19:07:01 +0100

    ✨ feat: problem No.206

---
+++ 206.reverse-linked-list.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=206 lang=typescript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+	let prev = null;
+	let current = head;
+	while (current) {
+		let next = current.next;
+		current.next = prev;
+		prev = current;
+		current = next;
+	}
+	return prev;
+    
+};
+// @lc code=end
+
