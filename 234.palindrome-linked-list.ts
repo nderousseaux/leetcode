e1446f3133693c54ca7979473d923ba46ef02e10
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-03-11 12:54:07 +0100

    ✨ feat: problem No.234

---
+++ 234.palindrome-linked-list.ts
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=234 lang=typescript
+ *
+ * [234] Palindrome Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function isPalindrome(head: ListNode | null): boolean {
+	let arr = [];
+	while (head) {
+		arr.push(head.val);
+		head = head.next;
+	}
+	let left = 0;
+	let right = arr.length - 1;
+	while (left < right) {
+		if (arr[left] !== arr[right]) {
+			return false;
+		}
+		left++;
+		right--;
+	}
+	return true;
+    
+};
+// @lc code=end
+
