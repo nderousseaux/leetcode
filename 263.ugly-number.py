c6995fdb30b74cd94db96ca23f2bffd53a7f91f1
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-03-15 13:47:07 +0100

    ✨ feat: problem No.263

---
+++ 263.ugly-number.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=263 lang=python3
+#
+# [263] Ugly Number
+#
+
+# @lc code=start
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        for i in [2, 3, 5]:
+            while n % i == 0:
+                n /= i
+        return n == 1
+# @lc code=end
+
