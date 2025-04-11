fcb8545d97c3072f641b21274d3d45b53bd14c49
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-04-11 10:00:57 +0200

    ✨ feat: problem No.415

---
+++ 415.add-strings.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=415 lang=python3
+#
+# [415] Add Strings
+#
+
+# @lc code=start
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        carry = 0
+        result = []
+        i, j = len(num1) - 1, len(num2) - 1
+
+        while i >= 0 or j >= 0 or carry:
+            digit1 = int(num1[i]) if i >= 0 else 0
+            digit2 = int(num2[j]) if j >= 0 else 0
+
+            total = digit1 + digit2 + carry
+            carry = total // 10
+            result.append(str(total % 10))
+
+            i -= 1
+            j -= 1
+
+        return ''.join(result[::-1])
+# @lc code=end
+
