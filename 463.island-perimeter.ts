ac9d2d6eed5998e50ebb92897eebaff762ef546c
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-04-22 09:32:34 +0200

    feat: problem No.463

---
+++ 463.island-perimeter.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=463 lang=typescript
+ *
+ * [463] Island Perimeter
+ */
+
+// @lc code=start
+function islandPerimeter(grid: number[][]): number {
+		let perimeter = 0;
+		const rows = grid.length;
+		const cols = grid[0].length;
+
+		for (let i = 0; i < rows; i++) {
+				for (let j = 0; j < cols; j++) {
+						if (grid[i][j] === 1) {
+								perimeter += 4;
+
+								if (i > 0 && grid[i - 1][j] === 1) perimeter -= 2;
+								if (j > 0 && grid[i][j - 1] === 1) perimeter -= 2;
+						}
+				}
+		}
+
+		return perimeter;
+};
+// @lc code=end
+
