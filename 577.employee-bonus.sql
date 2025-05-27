16821270c9b649081abec76ade14359c4d2f56dd
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-05-27 11:27:49 +0200

    feat: problem No.577

---
+++ 577.employee-bonus.sql
@@ -0,0 +1,18 @@
+--
+-- @lc app=leetcode id=577 lang=postgresql
+--
+-- [577] Employee Bonus
+--
+
+-- @lc code=start
+-- Write your MySQL query statement below
+SELECT e.name, b.bonus
+FROM Employee as e
+LEFT JOIN Bonus as b ON e.empId = b.empId
+WHERE b.bonus < 1000
+OR Bonus IS NULL
+;
+
+
+-- @lc code=end
+
