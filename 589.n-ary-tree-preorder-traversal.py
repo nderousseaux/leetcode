3ac9fad7ebd3a2e332c78b41cd2c728845dfef26
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-06-02 10:01:55 +0200

    feat: problem No.589

---
+++ 589.n-ary-tree-preorder-traversal.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=589 lang=python3
+#
+# [589] N-ary Tree Preorder Traversal
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        res = []
+
+        def through(node: 'Node'):
+            res.append(node.val)
+            for c in node.children:
+                through(c)
+
+        through(root)
+        return res 
+# @lc code=end
+
