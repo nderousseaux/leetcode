cc4b7c139b99de2233e08c408011d00d71ab3821
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-07-08 09:57:02 +0200

    feat: problem No.709

---
+++ 709.to-lower-case.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=709 lang=typescript
+ *
+ * [709] To Lower Case
+ */
+
+// @lc code=start
+function toLowerCase(s: string): string {
+    return s.toLowerCase()    
+};
+// @lc code=end
+
