1261a9620546e45e8a79a0ef9b4eef300998bdfa
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-09-02 10:23:35 +0200

    feat: problem No.1050

---
+++ 1050.actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,17 @@
+--
+-- @lc app=leetcode id=1050 lang=postgresql
+--
+-- [1050] Actors and Directors Who Cooperated At Least Three Times
+--
+
+-- @lc code=start
+-- Write your postgresql query statement below
+
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >= 3
+;
+
+-- @lc code=end
+
