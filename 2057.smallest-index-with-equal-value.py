a0bd14a7473914aef632c2695741ab8b00eed438
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2026-01-07 09:45:19 +0100

    ✨ feat: problem No.2057

---
+++ 2057.smallest-index-with-equal-value.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=2057 lang=python3
+#
+# [2057] Smallest Index With Equal Value
+#
+
+# @lc code=start
+class Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if i % 10 == nums[i]:
+                return i
+
+        return -1
+# @lc code=end
+
