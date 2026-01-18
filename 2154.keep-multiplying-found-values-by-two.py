0f305153c72d4c93c9f81cce02e20b97a370cb8a
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2026-01-18 18:58:37 +0100

    ✨ feat: problem No.2154

---
+++ 2154.keep-multiplying-found-values-by-two.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=2154 lang=python3
+#
+# [2154] Keep Multiplying Found Values by Two
+#
+
+# @lc code=start
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        while original in nums:
+            original *= 2
+
+        return original
+# @lc code=end
+
