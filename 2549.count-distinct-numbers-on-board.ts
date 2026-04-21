e0de7e354c97a66355c89d00678bb2ed4b18e47b
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2026-04-21 09:00:28 +0200

    ✨ feat: problem No.2549

---
+++ 2549.count-distinct-numbers-on-board.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=2549 lang=typescript
+ *
+ * [2549] Count Distinct Numbers on Board
+ */
+
+// @lc code=start
+function distinctIntegers(n: number): number {
+  return n > 1 ? n - 1 : 1;
+};
+// @lc code=end
