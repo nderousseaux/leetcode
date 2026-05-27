38fbcde12960b4e78c51294697c3dc35e5ed44a7
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2026-05-27 10:55:41 +0200

    ✨ feat: problem No.2769

---
+++ 2769.find-the-maximum-achievable-number.ts
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=2769 lang=typescript
+ *
+ * [2769] Find the Maximum Achievable Number
+ */
+
+// @lc code=start
+function theMaximumAchievableX(num: number, t: number): number {
+  return num + 2 * t;
+};
+// @lc code=end
